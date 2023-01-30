00fd510f851cafda421aded8a38dabb89ab17f40
Christian Oosterveen <c.oosterveen@sintlucasedu.nl>
2023-01-30 16:03:00 +0100

    main

--- Assets/Scripts/Thomas/PlayerMovement.cs
+++ Assets/Scripts/Thomas/PlayerMovement.cs
@@ -1,70 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Parameters")]
-    [SerializeField] private float speed;
-    
+    //Movement player
+    [Header("Speed")]
+    public float speed = 10;
 
-    
+    //Animation player
+    Animator animator;
+    string currentState;
+    const string IDLE = "Idle";
+    const string WALK = "Run";
 
-    
-
-    
-
-    [Header("Layers")]
-    [SerializeField] private LayerMask groundLayer;
-    
-
-    
-
-    private Rigidbody2D body;
-    private Animator anim;
-    private BoxCollider2D boxCollider;
-    
-    private float horizontalInput;
-
-    private void Awake()
+    private void Start()
     {
-        //Grab references for rigidbody and animator from object
-        body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        animator = gameObject.GetComponent<Animator>();
     }
 
-    private void Update()
+    void FixedUpdate()
     {
-        horizontalInput = Input.GetAxis("Horizontal");
+        float dir = Input.GetAxis("Horizontal");
+        transform.Translate(transform.right * dir * speed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
 
-        //Flip player when moving left-right
-        if (horizontalInput > 0.05f)
-            transform.localScale = new Vector3(5,5,5);
-        else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-5, 5, 5);
+        if (Input.GetButton("Horizontal"))
+        {
+            ChangeAnimationState(WALK);
+        }
+        if (!IsAnimtionPlaying(animator, currentState))
+        {
+            ChangeAnimationState(IDLE);
+        }
 
-        //Set animator parameters
-        anim.SetBool("run", horizontalInput != 0);
-        anim.SetBool("grounded", isGrounded());
-
-        //Jump
-        
-
-        
-
-        
-        
+        if (dir < 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        if (dir > 0)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
-    
-
-   
-
-
-    private bool isGrounded()
+    private void ChangeAnimationState(string newState)
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
-        return raycastHit.collider != null;
+        if (newState == currentState)
+        {
+            return;
+        }
+
+        animator.Play(newState);
+
+        currentState = newState;
     }
-    
-    
+
+    bool IsAnimtionPlaying(Animator animator, string stateName)
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName(stateName) && animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
